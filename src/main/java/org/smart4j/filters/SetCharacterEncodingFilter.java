bee672c55cc54513ec3a3b60f8baad308607e83c
dkw <1038459754@qq.com>
2017-08-17 09:57:16 +0800

    添加简单的事务

---
+++ src/main/java/org/smart4j/filters/SetCharacterEncodingFilter.java
@@ -0,0 +1,51 @@
+package org.smart4j.filters;
+
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * Created by dkw on 2017/8/17.
+ */
+public class SetCharacterEncodingFilter implements Filter {
+    protected String encoding = null;
+    protected FilterConfig filterConfig = null;
+    protected boolean ignore = true;
+
+    public void destroy() {
+        this.encoding = null;
+        this.filterConfig = null;
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response,
+                         FilterChain chain)
+            throws IOException, ServletException {
+        // Conditionally select and set the character encoding to be used
+        if (ignore || (request.getCharacterEncoding() == null)) {
+            String encoding = selectEncoding(request);
+            if (encoding != null)
+                request.setCharacterEncoding(encoding);
+        }
+        // Pass control on to the next filter
+        chain.doFilter(request, response);
+    }
+
+    public void init(FilterConfig filterConfig) throws ServletException {
+        this.filterConfig = filterConfig;
+        this.encoding = filterConfig.getInitParameter("encoding");
+        String value = filterConfig.getInitParameter("ignore");
+        if (value == null)
+            this.ignore = true;
+        else if (value.equalsIgnoreCase("true"))
+            this.ignore = true;
+        else if (value.equalsIgnoreCase("yes"))
+            this.ignore = true;
+        else
+            this.ignore = false;
+    }
+
+    protected String selectEncoding(ServletRequest request) {
+        return (this.encoding);
+    }
+}
+
